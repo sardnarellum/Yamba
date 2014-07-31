d1f857be2ff08d97787f9375d24489dc1c917591
sardnarellum <sardnarellum@gmail.com>
2014-07-31 16:40:27 +0200

    Up navigation implemented
    Only for inside up. TODO: for new task

--- app/src/main/java/com/andrasmuller/yamba/SettingsActivity.java
+++ app/src/main/java/com/andrasmuller/yamba/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.andrasmuller.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 /**
  * Created by andras on 2014.07.30..
@@ -12,11 +14,25 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         if (null == savedInstanceState) {
             SettingsFragment fragment = new SettingsFragment();
             getFragmentManager().beginTransaction()
                                 .add(android.R.id.content, fragment, fragment.getClass().getSimpleName())
                                 .commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/andrasmuller/yamba/StatusActivity.java
+++ app/src/main/java/com/andrasmuller/yamba/StatusActivity.java
@@ -2,13 +2,17 @@ package com.andrasmuller.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class StatusActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (null == savedInstanceState) {
             StatusFragment fragment = new StatusFragment();
@@ -24,5 +28,17 @@ public class StatusActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.status, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
